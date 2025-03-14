8ef5a7d2fc14efb34581d7e7f91043af28ac0b42
Matteo Filippini <34274990+Mattemn97@users.noreply.github.com>
2025-03-14 12:00:27 +0100

    Update Nemici.cpp

--- Nemici.cpp
+++ Nemici.cpp
@@ -1,4 +1,6 @@
 #include "Nemici.h"
+#include "Utils.h"
+#include <iostream>
 
 Scigacz::Scigacz() {
     x = 0;
@@ -7,11 +9,30 @@ Scigacz::Scigacz() {
     velocità = 1;
 }
 
+void Scigacz::crea(int x, int y) {
+    this->x = x;
+    this->y = y;
+    esiste = true;
+    gotoxy(x, y);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
+    std::cout << "&";  // Simbolo per il nemico che insegue
+}
+
 BiegaczPoz::BiegaczPoz() {
     x = 0;
     y = 0;
     vite = 2;
     direzione = 'd';  // Default: si muove a destra
+}
+
+void BiegaczPoz::crea(int x, int y, char direzione) {
+    this->x = x;
+    this->y = y;
+    this->direzione = direzione;
+    vite = 2;
+    gotoxy(x, y);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+    std::cout << "S";  // Simbolo per il nemico che si muove orizzontalmente
 }
 
 Randomer::Randomer() {
@@ -24,4 +45,18 @@ Randomer::Randomer() {
     vite = 2;
     movimentoRiuscito = false;
     velocità = 2;
+}
+
+void Randomer::crea(int x, int y, int area, int velocità) {
+    this->x = x;
+    this->y = y;
+    xMin = x - area;
+    xMax = x + area;
+    yMin = y - area;
+    yMax = y + area;
+    this->velocità = velocità;
+    vite = 2;
+    gotoxy(x, y);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 13);
+    std::cout << "%";  // Simbolo per il nemico che si muove casualmente
 }
