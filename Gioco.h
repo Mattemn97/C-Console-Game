6732bec1922c8fa6fe73b6e6be748cff581fc18d
Matteo Filippini <34274990+Mattemn97@users.noreply.github.com>
2025-03-14 12:39:52 +0100

    Update Gioco.h

--- Gioco.h
+++ Gioco.h
@@ -7,10 +7,13 @@
 
 class Gioco {
 public:
-    Gioco();
+    int difficolta;  // 1 = Facile, 2 = Medio, 3 = Difficile
+
+    Gioco(int livelloDifficolta);
     void aggiorna();
     bool verificaCollisione(int x, int y);
     void gestisciInput(char input);
+    void rilevaDanno();
 };
 
 #endif // GIOCO_H
