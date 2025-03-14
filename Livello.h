d5c0061aa93c2e4334365186741669617afb60eb
Matteo Filippini <34274990+Mattemn97@users.noreply.github.com>
2025-03-14 12:08:16 +0100

    Update Livello.h

--- Livello.h
+++ Livello.h
@@ -9,11 +9,13 @@ public:
     void caricaMappa(const std::string& nomeFile);
     void generaElementi();
     
-    // Nuove funzioni per generare gli oggetti di gioco
     void creaSkrzynie(int x, int y);
     void creaZbierajkeZycie(int x, int y);
     void creaZbierajkeMunizioni(int x, int y);
     void creaZbierajkeCheckPoint(int x, int y);
+    
+    // Nuova funzione per il boss
+    void creaBoss(int x, int y);
 };
 
 #endif // LIVELLO_H
