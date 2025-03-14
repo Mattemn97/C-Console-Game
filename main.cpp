ad481bbbc28ad798223fb3d5315c86243f271d45
Matteo Filippini <34274990+Mattemn97@users.noreply.github.com>
2025-03-14 12:41:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "Utils.h"
 
 void menu();
-void avviaGioco();
+void avviaGioco(int difficolta);
 
 int main() {
     SetWindow(113, 44);
@@ -13,30 +13,43 @@ int main() {
 
 void menu() {
     char scelta;
+    int difficolta = 2;  // Default: Medio
+
     do {
         system("CLS");
         std::cout << "=== MENU PRINCIPALE ===\n";
-        std::cout << "1. Avvia Gioco\n";
-        std::cout << "2. Esci\n";
+        std::cout << "1. Avvia Gioco (Facile)\n";
+        std::cout << "2. Avvia Gioco (Medio)\n";
+        std::cout << "3. Avvia Gioco (Difficile)\n";
+        std::cout << "4. Esci\n";
         std::cout << "Scelta: ";
         std::cin >> scelta;
 
         switch (scelta) {
             case '1':
-                avviaGioco();
+                difficolta = 1;
+                avviaGioco(difficolta);
                 break;
             case '2':
+                difficolta = 2;
+                avviaGioco(difficolta);
+                break;
+            case '3':
+                difficolta = 3;
+                avviaGioco(difficolta);
+                break;
+            case '4':
                 std::cout << "Uscita dal gioco...\n";
                 break;
             default:
                 std::cout << "Scelta non valida! Riprova.\n";
                 system("pause");
         }
-    } while (scelta != '2');
+    } while (scelta != '4');
 }
 
-void avviaGioco() {
+void avviaGioco(int difficolta) {
     system("CLS");
-    Gioco gioco;
-    gioco.aggiorna();  // Avvia il ciclo di gioco
+    Gioco gioco(difficolta);
+    gioco.aggiorna();
 }
